81d48a02b1c120959120b97ad799bf718e4f37d2
Muhammad <muhammadhullah23@gmail.com>
2021-03-21 16:49:42 -0400

    commit tree 6b7c3cf38670ff330c74e4632e47010ab954a8a1

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,24 @@
+class Artist
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        Song.all.map {|song| song.genre}
+      end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+    end
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
-
+class Doctor
+    attr_accessor :name, :appointments, :patient
+    @@all=[]
+    def initialize(name)
+      @name = name
+      @appointments = []
+      @@all << self
+    end
+    def self.all
+      @@all
+    end
+  
+    def new_appointment(date, patient)
+       appointment = Appointment.new(date, patient,  self)
+       @appointments << appointment
+       appointment
+    end
+    def appointments
+        Appointment.all.select {|appointment| appointment.doctor == self}
+      end
+  
+    def patients
+      @appointments.collect do |appointment|
+        appointment.patient
+      end
+    end
+  end
+  
+  
